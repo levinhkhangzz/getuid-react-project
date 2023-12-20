a3f041a5717bc004d530136deb0bba87fca4e65c
Aedotris ⛧ <127677696+levinhkhangzz@users.noreply.github.com>
2023-12-20 11:46:17 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -151,7 +151,7 @@ const App = () => {
 
           <div className="flex items-center justify-between mb-6">
             <button
-              className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md"
+              className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
               onClick={getUid}
               disabled={loading}
             >
